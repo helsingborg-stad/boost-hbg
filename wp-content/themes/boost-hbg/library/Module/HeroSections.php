dc1e61635d6f89d21a024058f51d44364b53d7cc
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-20 12:39:51 +0200

    Hero sections fields

--- wp-content/themes/boost-hbg/library/Module/HeroSections.php
+++ wp-content/themes/boost-hbg/library/Module/HeroSections.php
@@ -34,5 +34,145 @@ class HeroSections extends \Modularity\Module
             $paths[] = BOOST_PATH . 'templates/';
             return $paths;
         });
+
+        $this->fields();
+    }
+
+    public function fields()
+    {
+        if( function_exists('acf_add_local_field_group') ):
+
+            acf_add_local_field_group(array (
+                'key' => 'group_57174524697b9',
+                'title' => 'Hero sections',
+                'fields' => array (
+                    array (
+                        'key' => 'field_5717452be77f3',
+                        'label' => 'Sections',
+                        'name' => 'herosec-section',
+                        'type' => 'repeater',
+                        'instructions' => '',
+                        'required' => 1,
+                        'conditional_logic' => 0,
+                        'wrapper' => array (
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'collapsed' => '',
+                        'min' => 1,
+                        'max' => '',
+                        'layout' => 'block',
+                        'button_label' => 'Add Section',
+                        'sub_fields' => array (
+                            array (
+                                'key' => 'field_57174586e77f6',
+                                'label' => 'Icon SVG',
+                                'name' => 'icon_svg',
+                                'type' => 'file',
+                                'instructions' => 'Only .svg files is supported',
+                                'required' => 1,
+                                'conditional_logic' => 0,
+                                'wrapper' => array (
+                                    'width' => '',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'return_format' => 'array',
+                                'library' => 'all',
+                                'min_size' => '',
+                                'max_size' => '',
+                                'mime_types' => '',
+                            ),
+                            array (
+                                'key' => 'field_57174552e77f4',
+                                'label' => 'Icon background',
+                                'name' => 'icon_background',
+                                'type' => 'color_picker',
+                                'instructions' => '',
+                                'required' => 1,
+                                'conditional_logic' => 0,
+                                'wrapper' => array (
+                                    'width' => '50%',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '#000000',
+                            ),
+                            array (
+                                'key' => 'field_57174573e77f5',
+                                'label' => 'Icon foreground',
+                                'name' => 'icon_foreground',
+                                'type' => 'color_picker',
+                                'instructions' => '',
+                                'required' => 1,
+                                'conditional_logic' => 0,
+                                'wrapper' => array (
+                                    'width' => 50,
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '#FFFFFF',
+                            ),
+                            array (
+                                'key' => 'field_571745ace77f7',
+                                'label' => 'Title',
+                                'name' => 'title',
+                                'type' => 'text',
+                                'instructions' => '',
+                                'required' => 0,
+                                'conditional_logic' => 0,
+                                'wrapper' => array (
+                                    'width' => 50,
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '',
+                                'placeholder' => '',
+                                'prepend' => '',
+                                'append' => '',
+                                'maxlength' => '',
+                                'readonly' => 0,
+                                'disabled' => 0,
+                            ),
+                            array (
+                                'key' => 'field_571745c9e77f8',
+                                'label' => 'Link URL',
+                                'name' => 'target_url',
+                                'type' => 'url',
+                                'instructions' => '',
+                                'required' => 1,
+                                'conditional_logic' => 0,
+                                'wrapper' => array (
+                                    'width' => 50,
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '',
+                                'placeholder' => '',
+                            ),
+                        ),
+                    ),
+                ),
+                'location' => array (
+                    array (
+                        array (
+                            'param' => 'post_type',
+                            'operator' => '==',
+                            'value' => 'mod-herosection',
+                        ),
+                    ),
+                ),
+                'menu_order' => 0,
+                'position' => 'normal',
+                'style' => 'default',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => 1,
+                'description' => '',
+            ));
+
+            endif;
     }
 }
