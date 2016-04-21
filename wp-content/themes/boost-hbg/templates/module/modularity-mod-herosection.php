542ba2d062b47fa3226fd2c0945b1416bbd932b2
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-21 11:18:51 +0200

    Hero section style updates

--- wp-content/themes/boost-hbg/templates/module/modularity-mod-herosection.php
+++ wp-content/themes/boost-hbg/templates/module/modularity-mod-herosection.php
@@ -2,21 +2,27 @@
     $fields = get_fields($module->ID);
 ?>
 <div class="mod-herosection">
-    <?php foreach ($fields['herosec-section'] as $section) : ?>
-        <a href="<?php echo $section['target_url']; ?>">
-            <style scoped>
-                svg,
-                svg * {
-                    fill: <?php echo $section['icon_foreground']; ?>;
-                }
-            </style>
-            <div class="mod-herosection-icon">
-                <div class="mod-herosection-icon-symbol" style="background-color:<?php echo $section['icon_background']; ?>">
-                    <?php echo \Municipio\Helper\Svg::extract($section['icon_svg']['url']); ?>
-                </div>
-            </div>
+    <div class="container">
+        <div class="grid-xs-12 mod-herosection-table">
+            <?php foreach ($fields['herosec-section'] as $section) : ?>
+                <section>
+                    <a href="<?php echo $section['target_url']; ?>">
+                        <style scoped>
+                            svg,
+                            svg * {
+                                fill: <?php echo $section['icon_foreground']; ?>;
+                            }
+                        </style>
+                        <div class="mod-herosection-icon">
+                            <div class="mod-herosection-icon-symbol" style="background-color:<?php echo $section['icon_background']; ?>">
+                                <?php echo \Municipio\Helper\Svg::extract($section['icon_svg']['url']); ?>
+                            </div>
+                        </div>
 
-            <span class="mod-herosection-title"><?php echo $section['title']; ?></span>
-        </a>
-    <?php endforeach; ?>
+                        <span class="mod-herosection-title"><?php echo $section['title']; ?></span>
+                    </a>
+                </section>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
