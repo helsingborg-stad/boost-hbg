4a93e2621c4a39ffff3628c42e7aae83a0cabbbb
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-05-30 15:24:53 +0200

    Better calculation of animation start value.

--- wp-content/themes/boost-hbg/assets/dist/js/app.js
+++ wp-content/themes/boost-hbg/assets/dist/js/app.js
@@ -6,23 +6,20 @@ jQuery(function() {
     targetWrapper       = 'main [class^="modularity-mod-"] [class^="grid-"], .social-feed > li, .sidebar-footer-area > [class^="grid-"], .modularity-mod-table .box-panel, div:not(.hero) > .modularity-mod-slider .slider, .modularity-mod-inlaylist > .box-panel, .modularity-mod-iframe > iframe, .modularity-mod-contact > .box-card',
     target;
 
-    //On load visibility
-    jQuery(targetWrapper).css("visibility", "visible");
-
     //Scroll event
     $window.on('scroll', revealOnScroll);
 
     //Run scroll reveal
     function revealOnScroll() {
         var scrolled        = $window.scrollTop(),
-        win_height_padded   = $window.height() * 1.1;
+        win_height_padded   = $window.height() * 0.98;
 
         $(targetWrapper + ":not(.animated)").each(function() {
             var $this = $(this),
                 offsetTop = $this.offset().top;
 
             if (scrolled + win_height_padded > offsetTop) {
-              $this.addClass('animated fadeInUp');
+              $this.css("visibility", "visible").addClass('animated fadeInUp');
             }
         });
 
