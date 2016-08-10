066eee17ea2fb41e57439f0b8d61a3cd2d7ebe13
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-08-10 08:43:10 +0200

    Updated animation function

--- wp-content/themes/boost-hbg/assets/dist/js/app.js
+++ wp-content/themes/boost-hbg/assets/dist/js/app.js
@@ -1,4 +1,4 @@
-jQuery(function() {
+$(function(jQuery) {
 
     //Declarations
     var $window         = $(window),
--- wp-content/themes/boost-hbg/library/Theme/Animate.php
+++ wp-content/themes/boost-hbg/library/Theme/Animate.php
@@ -11,10 +11,12 @@ class Animate
 
     public function makeAnimatedObjectVisible()
     {
-        echo '<noscript>';
-            echo '<style>';
-                echo 'main [class^="modularity-mod-"] [class^="grid-"], .social-feed > li, .sidebar-footer-area > [class^="grid-"], .modularity-mod-table .box-panel, div:not(.hero) > .modularity-mod-slider .slider, .modularity-mod-inlaylist > .box-panel, .modularity-mod-iframe > iframe, .modularity-mod-contact > .box-card {visibility: visible;}';
-            echo '</style>';
-        echo '</noscript>';
+        if(!is_admin()) {
+            echo '<noscript>';
+                echo '<style>';
+                    echo 'main [class^="modularity-mod-"] [class^="grid-"], .social-feed > li, .sidebar-footer-area > [class^="grid-"], .modularity-mod-table .box-panel, div:not(.hero) > .modularity-mod-slider .slider, .modularity-mod-inlaylist > .box-panel, .modularity-mod-iframe > iframe, .modularity-mod-contact > .box-card {visibility: visible;}';
+                echo '</style>';
+            echo '</noscript>';
+        }
     }
 }
