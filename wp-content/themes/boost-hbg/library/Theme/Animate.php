50a6876fbea80f2a121237f875df989e340f2e69
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-08-10 08:47:25 +0200

    Temporary animation fix.

--- wp-content/themes/boost-hbg/library/Theme/Animate.php
+++ wp-content/themes/boost-hbg/library/Theme/Animate.php
@@ -11,12 +11,12 @@ class Animate
 
     public function makeAnimatedObjectVisible()
     {
-        if(!is_admin()) {
-            echo '<noscript>';
+        //if(!is_admin()) {
+            //echo '<noscript>';
                 echo '<style>';
                     echo 'main [class^="modularity-mod-"] [class^="grid-"], .social-feed > li, .sidebar-footer-area > [class^="grid-"], .modularity-mod-table .box-panel, div:not(.hero) > .modularity-mod-slider .slider, .modularity-mod-inlaylist > .box-panel, .modularity-mod-iframe > iframe, .modularity-mod-contact > .box-card {visibility: visible;}';
                 echo '</style>';
-            echo '</noscript>';
-        }
+            //echo '</noscript>';
+        //}
     }
 }
