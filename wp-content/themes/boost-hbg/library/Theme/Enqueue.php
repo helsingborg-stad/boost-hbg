865b940812446fbca85ea6831b31c1808ba382e5
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-08-11 11:12:20 +0200

    Enqueue script

--- wp-content/themes/boost-hbg/library/Theme/Enqueue.php
+++ wp-content/themes/boost-hbg/library/Theme/Enqueue.php
@@ -18,8 +18,6 @@ class Enqueue
      */
     public function style()
     {
-        wp_register_style('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/css/hbg-prime.min.css', '', '1.0.0');
-        wp_enqueue_style('hbg-prime');
         wp_enqueue_style('boost-css', get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css', '', filemtime(get_stylesheet_directory() . '/assets/dist/css/app.min.css'));
     }
 
@@ -30,9 +28,6 @@ class Enqueue
     public function script()
     {
         wp_enqueue_script('boost-js', get_stylesheet_directory_uri(). '/assets/dist/js/app.min.js', '', filemtime(get_stylesheet_directory() . '/assets/dist/js/app.min.js'), true);
-
-        wp_register_script('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/js/hbg-prime.min.js', '', '1.0.0', true);
-        wp_enqueue_script('hbg-prime');
     }
 
     /**
