e06dc840c40da2ea4b211dc74242617afc75b079
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-05-14 11:15:31 +0200

    add dot env lib

--- wp-content/themes/boost-hbg/functions.php
+++ wp-content/themes/boost-hbg/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 define('BOOST_PATH', get_stylesheet_directory() . '/');
 
 //Include vendor files
@@ -8,6 +11,7 @@ if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
 }
 
 require_once BOOST_PATH . 'library/Vendor/Psr4ClassLoader.php';
+
 $loader = new BoostHBG\Vendor\Psr4ClassLoader();
 $loader->addPrefix('BoostHBG', BOOST_PATH . 'library');
 $loader->addPrefix('BoostHBG', BOOST_PATH . 'source/php/');
