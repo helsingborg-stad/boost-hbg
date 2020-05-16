697d196dd380e20c7c37b014c1466b70b34afe3f
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-05-16 08:25:36 +0200

    add filter top support svg in sliders

--- wp-content/themes/boost-hbg/library/App.php
+++ wp-content/themes/boost-hbg/library/App.php
@@ -8,8 +8,18 @@ class App
         new \BoostHBG\Theme\Enqueue();
         new \BoostHBG\Theme\Filters();
         new \BoostHBG\Theme\Color();
-        new \BoostHBG\Theme\Animate();
+        // new \BoostHBG\Theme\Animate();
 
-        new \BoostHBG\Module\HeroSections();
+        // new \BoostHBG\Module\HeroSections();
+
+        // Enable SVG in sliders
+        add_filter('acf/load_field/key=field_56a5ed2f398dc', [$this, 'addSvgSupport']);
+    }
+
+    public function addSvgSupport($field)
+    {
+        $field['mime_types'] .= ',svg';
+        return $field;
     }
 }
+
