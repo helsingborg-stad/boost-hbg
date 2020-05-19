f300e7cf813e40a35c2e834c637d03999ecbb76b
Dan Nilsson <dan.nilsson@helsingborg.se>
2020-05-19 22:35:48 +0200

    add company logos @ footer

--- wp-content/themes/boost-hbg/views/partials/footer.blade.php
+++ wp-content/themes/boost-hbg/views/partials/footer.blade.php
@@ -15,24 +15,28 @@
       </div>
     @endif
 
-    @if (have_rows('footer_icons_repeater', 'option'))
-      <div class="grid social-icon-list">
-        <div class="grid-xs-12">
-          <ul class="icons-list">
-            @foreach(get_field('footer_icons_repeater', 'option') as $link)
-              <li>
-                <a class="link-item-light" href="{{ $link['link_url'] }}" target="_blank">
-                  {!! $link['link_icon'] !!}
-                  @if (isset($link['link_title']))
-                    <span class="sr-only">{{ $link['link_title'] }}</span>
-                  @endif
-                </a>
-              </li>
-            @endforeach
-          </ul>
-        </div>
-      </div>
-    @endif
-  </div>
 
+      <div class="grid">
+        <div class="grid-md-6 grid-sm-12 company-logos">
+          <img class="logo" src="{!! get_stylesheet_directory_uri() . '/assets/images/rs_white.png' !!}" alt="">
+        </div>
+        @if (have_rows('footer_icons_repeater', 'option'))
+          <div class="grid-md-6 grid-sm-12 social-icon-list">
+            <ul class="icons-list">
+              @foreach(get_field('footer_icons_repeater', 'option') as $link)
+                <li>
+                  <a class="link-item-light" href="{{ $link['link_url'] }}" target="_blank">
+                    {!! $link['link_icon'] !!}
+                    @if (isset($link['link_title']))
+                      <span class="sr-only">{{ $link['link_title'] }}</span>
+                    @endif
+                  </a>
+                </li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
+      </div>
+
+  </div>
 </footer>
